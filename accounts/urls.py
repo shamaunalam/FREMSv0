1b5138849906e3f909c48bad96c1e7fbe9dc70af
Shamaun <shamaunalam@gmail.com>
2020-12-28 21:54:10 +0530

    fifth commit - added login logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home)
+    path('',views.Dashboard,name='dashboard'),
+    path('login',views.Login,name='login'),
+    path('logout',views.Logout,name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render,HttpResponse
-from django.contrib.auth import authenticate
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
-def home(request):
-    if request.user.is_authenticated:
+def Dashboard(request):
 
+    if request.user.is_authenticated:
         return render(request,'base.html')
     else:
-        return HttpResponse('<center><h1>Please Login First</h1><center>')
+        return redirect('login')
+
+def Login(request):
+    if request.method == 'POST':
+        EmpId = request.POST['EmpId']
+        password = request.POST['password']
+
+        user = authenticate(request,EmpId=EmpId,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('dashboard')
+    else:
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+        else:
+            return render(request,'login.html')
+
+def Logout(request):
+    if request.user is not None:
+        logout(request)
+        return redirect('login')
